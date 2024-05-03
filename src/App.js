6eb06b27920875baa4aaa5dc8816b1d1be6ce230
Mario Montante <mariom0092@gmail.com>
2024-05-03 03:17:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [sourceLanguage, setSourceLanguage] = useState('en-US');
+  const [targetLanguage, setTargetLanguage] = useState('it-IT');
+  const [translatedText, setTranslatedText] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const translateText = async () => {
+    try {
+      const response = await fetch('URL_ENDPOINT', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          text: inputText,
+          'source-lang': sourceLanguage,
+          'target-lang': targetLanguage,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
+      const data = await response.json();
+      setTranslatedText(data.translation);
+      setErrorMessage('');
+    } catch (error) {
+      console.error(error);
+      setTranslatedText('');
+      setErrorMessage('Error: Failed to translate text');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Translate</h1>
       </header>
+      <div className='Translator'>
+        <div className='translateArea'>
+          <select
+            value={targetLanguage}
+            onChange={e => setTargetLanguage(e.target.value)}
+          >
+            <option value='EN'>English</option>
+            <option value='IT'>Italiano</option>
+          </select>
+          <textarea
+            value={inputText}
+            onChange={e => setInputText(e.target.value)}
+            placeholder='Enter text to translate...'
+          />
+          <button onClick={translateText}>Translate</button>
+        </div>
+        <div className='TranslatedText'>
+          <h2>Translated Text</h2>
+          <textarea
+            value={translatedText}
+            readOnly
+            placeholder='Translated text will appear here...'
+          />
+        </div>
+      </div>
     </div>
   );
 }
